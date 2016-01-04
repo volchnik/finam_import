65d683dacc15fe3055aa397567520028269bc6c0
NIKITA VOLCHEGORSKIY <volchnik@gmail.com>
2016-01-04 15:11:58 +0500

    Добавлена упаковка котировок в секундные бары, работа с репозиторием котировок

--- import.py
+++ import.py
@@ -3,18 +3,26 @@ import datetime
 import os
 import os.path
 import json
- 
-def downloadStock(code_stock, date, json_charts_data):
-  if 12 <= date.month or date.month <= 2 :
-    stock_postfix = "H";
-  elif 3 <= date.month <= 5:
-    stock_postfix = "M";
-  elif 6 <= date.month <= 8:
-    stock_postfix = "U";
+import sys
+from package import package_quote 
+
+def get_stock_key(code_stock, date):
+  if code_stock == 'RI':
+    if 12 <= date.month or date.month <= 2 :
+      stock_postfix = "H";
+    elif 3 <= date.month <= 5:
+      stock_postfix = "M";
+    elif 6 <= date.month <= 8:
+      stock_postfix = "U";
+    else:
+      stock_postfix = "Z";
+      
+    return code_stock + stock_postfix + str(date.year)[3:]
   else:
-    stock_postfix = "Z";
-    
-  code_stock_key = code_stock + stock_postfix + str(date.year)[3:]
+    return code_stock
+ 
+def download_stock(code_stock, date, json_charts_data):
+  code_stock_key = get_stock_key(code_stock, date)
 
   url = 'http://195.128.78.52/{5}_{0}{1}{3}_{0}{1}{3}.txt?market=17&em={6}&code={5}&df={4}&mf={2}&yf=20{0}&from={3}.{1}.20{0}&dt={4}&mt={2}&yt=20{0}&to={3}.{1}.20{0}&p=1&f={5}_{0}{1}{3}_{0}{1}{3}&e=.txt&cn={5}&dtf=1&tmf=1&MSOR=1&mstime=on&mstimever=1&sep=3&sep2=1&datf=6'.format(str(date.year)[2:], str(date.month).rjust(2, '0'), date.month - 1, str(date.day).rjust(2, '0'), date.day, code_stock_key, json_charts_data[code_stock_key])
    
@@ -33,11 +41,13 @@ def downloadStock(code_stock, date, json_charts_data):
   data = urllib2.urlopen(request).read()
   if not os.path.exists("./data/" + code_stock):
     os.makedirs("./data/" + code_stock)
-  file_name = "./data/{3}/{4}_{0}{1}{2}_{0}{1}{2}.txt".format(str(date.year)[2:], str(date.month).rjust(2, '0'), str(date.day).rjust(2, '0'), code_stock, code_stock + stock_postfix + str(date.year)[3:])
-  with open(file_name, "wb") as code :
+  file_name = "{3}/{4}_{0}{1}{2}_{0}{1}{2}.txt".format(str(date.year)[2:], str(date.month).rjust(2, '0'), str(date.day).rjust(2, '0'), code_stock, code_stock_key)
+  with open('./data/' + file_name, "wb") as code :
     code.write(data)
-  if os.path.getsize(file_name) < 1000:
-    os.remove(file_name)
+  if os.path.getsize('./data/' + file_name) < 1000:
+    os.remove('./data/' + file_name)
+  else:
+    package_quote(file_name)
   return
 
 with open('icharts.json') as json_data_file:    
@@ -45,6 +55,8 @@ with open('icharts.json') as json_data_file:
   
 json_charts_data_dict = dict(zip(json_charts_data["aEmitentCodes"], json_charts_data["aEmitentIds"]))
 
-date_start = datetime.date(2010, 3, 12).toordinal();
-for current_date in range(date_start, date_start + 6):
-  downloadStock("RI", datetime.date.fromordinal(current_date), json_charts_data_dict);
+date_start = datetime.date(int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])).toordinal()
+quote_list = sys.argv[1].split(',')
+for current_date in range(date_start, date_start + int(sys.argv[5])):
+  for quote in quote_list:
+    download_stock(quote, datetime.date.fromordinal(current_date), json_charts_data_dict)
---
+++ package.py
@@ -0,0 +1,25 @@
+import gzip
+import collections
+import sys
+import os
+import os.path
+import git
+
+def package_quote(quote_path):
+  print "packaging " + quote_path
+  quote_dict = {}
+  volume_dict = {}
+  with gzip.open('data/' + quote_path, 'r') as f:
+    for line in f:
+      line_list = line.rstrip('\n').split(';')
+      quote_dict[line_list[2] + line_list[3]] = float(line_list[4]);
+      volume_dict[line_list[2] + line_list[3]] = volume_dict[line_list[2] + line_list[3]] + int(line_list[5]) if (line_list[2] + line_list[3] in volume_dict) else int(line_list[5]);
+
+  if not os.path.exists("stock_quotes/data_seconds/" + quote_path.split('/')[0]):
+    os.makedirs("stock_quotes/data_seconds/" + quote_path.split('/')[0])
+  with gzip.open('stock_quotes/data_seconds/' + quote_path, 'w') as f:
+    for key, val in sorted(quote_dict.items()):
+      f.write(key + ';' + str(val) + ';' + str(volume_dict[key]) + '\n')
+  repo = git.Repo('stock_quotes/')
+  print repo.git.add('data_seconds/' + quote_path)
+  print repo.git.commit(m='added ' + '/data_seconds/' + quote_path)
